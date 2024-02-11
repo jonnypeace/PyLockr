04ac452495c72abd3f80b1bbef77d621c03aae67
jonnypeace <royalpython2@hotmail.com>
2024-02-11 16:24:43 +0000

    better docstrings

--- app/utils/security.py
+++ app/utils/security.py
@@ -17,12 +17,21 @@ def get_secure_key():
 
 def encrypt_data(data):
     '''
-    Encrypts notes and passwords for password manager
+    Encrypts notes and passwords for the password manager.
+
+    This function applies an additional layer of encryption using Fernet
+    on top of the SQLCipher database encryption. The encrypted data is stored
+    in the SQLCipher-encrypted database, effectively double-encrypting it.
     '''
     return current_app.config['CIPHER_SUITE'].encrypt(data.encode())
 
 def decrypt_data(encrypted_data):
     '''
-    Decrypts passwords and notes for password manager
+    Decrypts passwords and notes for the password manager.
+
+    This function decrypts data that was previously encrypted with Fernet,
+    which is stored in a SQLCipher-encrypted database. This means the data
+    is decrypted by Fernet first, then automatically decrypted by SQLCipher
+    as it's read from the database, reversing the double encryption process.
     '''
     return current_app.config['CIPHER_SUITE'].decrypt(encrypted_data).decode()
