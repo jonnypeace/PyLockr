8a6aa59f4cd62c7d8b2b3836c5bdfd5b5dc3fa75
jonnypeace <royalpython2@hotmail.com>
2024-02-11 00:24:49 +0000

    minor tidy up

--- config.py
+++ config.py
@@ -10,9 +10,6 @@ class Config:
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=int(os.environ.get('SESSION_TIMEOUT', 30)))
     DB_PATH = os.path.join(os.getcwd(), 'database/users.db')
     KEY = os.environ.get('FERNET_KEY')
-    # CIPHER_SUITE = Fernet(os.environ.get('FERNET_KEY'))
-
-    # DB_PATH = os.environ.get('DATABASE_URL', 'sqlite:///database/users.db')
     FERNET_KEY = os.environ.get('FERNET_KEY')
     
     if FERNET_KEY:
