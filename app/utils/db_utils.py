46edc4d3d25cccb8110dadd864a0fdf6ff06ab94
jonnypeace <royalpython2@hotmail.com>
2024-02-11 00:26:04 +0000

    blueprint for importing CSV file from vaultwarden

--- app/utils/db_utils.py
+++ app/utils/db_utils.py
@@ -3,6 +3,9 @@
 from pysqlcipher3 import dbapi2 as sqlite
 import os
 from flask import current_app
+from app.utils.security import *
+from app.utils.db_utils import *
+from html_sanitizer import Sanitizer
 
 def get_db_connection(passphrase):
     '''
@@ -32,7 +35,6 @@ def setup_db():
     cursor = conn.cursor()
 
     passphrase = os.environ.get('SQLCIPHER_KEY')
-    print(passphrase)
     if not passphrase:
         raise ValueError("SQLCIPHER_KEY is not set in the environment variables.")
     
@@ -56,3 +58,30 @@ def setup_db():
 
     conn.commit()
     conn.close()
+
+# def import_csv(filepath: str):
+#     import csv
+#     from pathlib import Path
+
+#     sanitizer = Sanitizer()
+
+#     # Retrieve the secure passphrase
+#     secure_key = get_secure_key()
+    
+#     # Connect to the encrypted database (SQLCipher) using the secure key
+#     conn = get_db_connection(secure_key)
+#     c = conn.cursor()
+
+#     if Path(filepath).exists():
+#         with open(filepath, mode='r') as file:
+#             csv_reader = csv.reader(file)
+#             for row in csv_reader:
+#                 name, username = row[3], row[8]
+#                 encrypted_pass = encrypt_data(sanitizer.sanitize(row[9]))
+#                 encrypted_notes = encrypt_data(sanitizer.sanitize(row[4]))
+#                 # Insert new password into the passwords table
+#                 c.execute('INSERT INTO passwords (user_id, name, username, encrypted_password, notes) VALUES (?, ?, ?, ?, ?)', 
+#                         (session['user_id'], name, username, encrypted_pass, encrypted_notes))
+
+#         conn.commit()
+#         conn.close()
