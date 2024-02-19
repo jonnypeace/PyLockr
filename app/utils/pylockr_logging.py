ef2dc8d84c086fa4d982504a02fd5956b881f827
jonnypeace <royalpython2@hotmail.com>
2024-02-19 23:25:48 +0000

    limit the number of handlers

--- app/utils/pylockr_logging.py
+++ app/utils/pylockr_logging.py
@@ -15,27 +15,30 @@ class PyLockrLogs:
         logger.error('This is an error message.')
     '''
 
-    def __init__(self, name='flask_app', log_file='flask_app.log', level=logging.INFO):
+    def __init__(self, name=__name__, log_file='flask.log', level=logging.INFO):
         # Create a logger
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
-        
-        # Create a file handler for writing logs to a file
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(level)
-        
-        # Create a stream handler for writing logs to the console
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(level)
-        
-        # Create a formatter and set it for both handlers
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
-        
-        # Add handlers to the logger
-        self.logger.addHandler(file_handler)
-        self.logger.addHandler(stream_handler)
+
+        # Check if the logger already has handlers to prevent adding them again
+        if not self.logger.handlers:
+            self.logger.setLevel(level)
+            
+            # Create a file handler for writing logs to a file
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setLevel(level)
+            
+            # Create a stream handler for writing logs to the console
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(level)
+            
+            # Create a formatter and set it for both handlers
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            file_handler.setFormatter(formatter)
+            stream_handler.setFormatter(formatter)
+            
+            # Add handlers to the logger
+            self.logger.addHandler(file_handler)
+            self.logger.addHandler(stream_handler)
         
     def info(self, message):
         '''
