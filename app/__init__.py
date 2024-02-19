907ca4247053f292877cd9853299fca49203ad4a
jonnypeace <royalpython2@hotmail.com>
2024-02-19 23:28:29 +0000

    rate limiting added

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,14 @@ from .main import main as main_blueprint
 from .auth import auth as auth_blueprint
 from config import Config
 
+from .utils.extensions import limiter  # Import the limiter
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    # Initialize Flask-Limiter with the app
+    limiter.init_app(app)
     app.register_blueprint(main_blueprint)
     # app.register_blueprint(auth_blueprint, url_prefix='/auth') ####### Consider for future
     app.register_blueprint(auth_blueprint)
-
     return app
