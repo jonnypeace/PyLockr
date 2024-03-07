c9dda2fb01f0c78a263caa92cec9d3a87fe390f3
jonnypeace <royalpython2@hotmail.com>
2024-03-07 21:25:23 +0000

    included example .env file, updated generate_key for .env example

--- generate_key.py
+++ generate_key.py
@@ -14,28 +14,24 @@ def generate_secret_key(length=24):
 
 def generate_keys():
     # Example usage:
-    flask_secret_key = generate_secret_key()
-    # Generate a new Fernet key
-    key = Fernet.generate_key()
+    fernet_key = Fernet.generate_key()
     # Decode the key to convert from bytes to string
-    fernet_decoded_key = key.decode()
-    sqlcipher_key = generate_password(32)
+    fernet_decoded_key = fernet_key.decode()
+
     print(f'''
-          If adding to your .bashrc file:
-          export SECRET_KEY='{flask_secret_key}'
-          export SQLCIPHER_KEY='{sqlcipher_key}'
-          export FERNET_KEY='{fernet_decoded_key}'
-          export REDIS_PASSWORD={generate_secret_key()}
-          
-          or .env file (better if using a docker build):
-          SECRET_KEY='{flask_secret_key}
-          SQLCIPHER_KEY='{sqlcipher_key}'
-          FERNET_KEY='{fernet_decoded_key}'
-          REDIS_PASSWORD={generate_secret_key()}
-          
-          Please Backup These keys, especially the SQLCIPHER_KEY which encrypts the database,
-          and the FERNET_KEY which applies another layer and encrypts passwords and notes.
-            ''')
+        Some randomly generated keys you can use for your
+        .env file:
+
+        APP_SECRET_KEY='{generate_secret_key(32)}'
+        FERNET_KEY='{fernet_decoded_key}'
+        REDIS_PASSWORD='{generate_password(32)}'
+        MYSQL_ROOT='{generate_password(32)}'
+        MYSQL_PASSWORD='{generate_password(32)}'
+        GPG_PASSPHRASE='{generate_password(32)}'
+
+        Please Backup These keys, they are especially important for your password encryption,
+        and the FERNET_KEY which applies another layer and encrypts passwords and notes.
+    ''')
 
 if __name__ == "__main__":
     generate_keys()
