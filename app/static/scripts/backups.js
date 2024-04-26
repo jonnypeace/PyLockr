4fcd631a375361acb457b6497a19dd059596c9e0
jonnypeace <royalpython2@hotmail.com>
2024-04-26 21:37:23 +0100

    rearranging, paving the way for upload with encrypted csv

--- app/static/scripts/backups.js
+++ app/static/scripts/backups.js
@@ -1,3 +1,9 @@
+import { base64ToArrayBuffer, reEncryptDEKWithSharedSecret, importServerPublicKey,
+         arrayBufferToBase64, keyPairGenerate, getSharedSecret, deriveAESKeyFromSharedSecret,
+         keyExchangeShare, finalExchange, getDek, getEdek, decryptData,
+         encryptStringWithAesGcm, importAesKeyFromBuffer} from './utils.js';
+
+
 function showToast(message, duration = 8000) {
     const container = document.getElementById('toast-container') || createToastContainer();
     const toast = document.createElement('div');
@@ -22,9 +28,81 @@ function createToastContainer() {
     return container;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    let isFormSubmitted = false; 
     const backupForm = document.getElementById('backupForm'); // Ensure your form has this ID
     backupForm.addEventListener('submit', function(e) {
-        showToast('Please test backup with the password you provided');
+        if (!isFormSubmitted) {
+            e.preventDefault();
+            showToast('Please test backup with the password you provided');
+            isFormSubmitted = true;
+            form.submit();
+        }
     });
 });
+
+async function encryptFileWithAesGcm(aesKey, file) {
+    const fileReader = new FileReader();
+
+    // Create a Promise to handle reading the file
+    const arrayBuffer = await new Promise((resolve, reject) => {
+        fileReader.onload = () => resolve(fileReader.result);
+        fileReader.onerror = () => reject(fileReader.error);
+        fileReader.readAsArrayBuffer(file);
+    });
+
+    // Generate a random Initialization Vector (IV) for AES-GCM
+    const iv = window.crypto.getRandomValues(new Uint8Array(12)); // 12 bytes IV for AES-GCM
+
+    try {
+        // Encrypt the ArrayBuffer with the AES key
+        const encryptedData = await window.crypto.subtle.encrypt(
+            { name: "AES-GCM", iv },
+            aesKey,
+            arrayBuffer // ArrayBuffer of file data
+        );
+
+        return { encryptedData, iv };
+    } catch (error) {
+        console.error("Failed to encrypt file:", error);
+        throw error; // Rethrow to handle the error externally
+    }
+}
+
+
+async function encryptAndSendFile(aesKey, file) {
+    const { encryptedData, iv } = await encryptFileWithAesGcm(aesKey, file);
+
+    // Convert ArrayBuffer to Base64
+    function bufferToBase64(buffer) {
+        let binary = '';
+        let bytes = new Uint8Array(buffer);
+        let len = bytes.byteLength;
+        for (let i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+
+    // Prepare data to send
+    const encryptedDataB64 = bufferToBase64(encryptedData);
+    const ivB64 = bufferToBase64(iv);
+
+    // Data to send as JSON
+    const dataToSend = JSON.stringify({
+        encryptedData: encryptedDataB64,
+        iv: ivB64
+    });
+
+    // Fetch API to send data to the server
+    fetch('YOUR_FLASK_SERVER_ENDPOINT', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: dataToSend
+    })
+    .then(response => response.json())
+    .then(data => console.log('Response from server:', data))
+    .catch(error => console.error('Error sending encrypted data:', error));
+}
--- app/static/scripts/passwords.js
+++ app/static/scripts/passwords.js
@@ -1,6 +1,8 @@
 import { base64ToArrayBuffer, reEncryptDEKWithSharedSecret, importServerPublicKey,
-         arrayBufferToBase64, keyPairGenerate, getSharedSecret, keyExchangeShare,
-        deriveAESKeyFromSharedSecret} from './utils.js';
+         arrayBufferToBase64, keyPairGenerate, getSharedSecret, deriveAESKeyFromSharedSecret,
+         keyExchangeShare, finalExchange, getDek, getEdek, decryptData,
+         encryptStringWithAesGcm, importAesKeyFromBuffer} from './utils.js';
+
 
 document.addEventListener('DOMContentLoaded', function () {
     var generateButton = document.getElementById('generatePasswordButton');
@@ -60,8 +62,6 @@ async function togglePasswordVisibility() {
                     const passArr = base64ToArrayBuffer(passwordField.value);
                     const passwordDecrypt = await decryptData(dek.dek, passArr, ivArr);
                     const password = new TextDecoder().decode(passwordDecrypt);
-                    // const password = base64DecodeUtf8(passwordB64);
-                    console.log(password)
                     passwordField.value = password;
                     passwordVisibilityToggled = true;
                     passwordField.type = 'text';
@@ -79,23 +79,6 @@ async function togglePasswordVisibility() {
     }
 }
 
-function base64DecodeUtf8(base64) {
-    // Decode Base64 to a binary string
-    const binaryString = atob(base64);
-
-    // Convert binary string to a character-number array
-    const charCodeArray = Array.from(binaryString, c => c.charCodeAt(0));
-
-    // Convert char code array to a byte array
-    const byteArray = new Uint8Array(charCodeArray);
-
-    // Decode byte array to a UTF-8 string
-    const text = new TextDecoder('utf-8').decode(byteArray);
-
-    return text;
-}
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
     let isFormSubmitted = false; // Flag to prevent infinite submission loop
@@ -105,7 +88,6 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault();
             if ((passwordVisibilityToggled === true && formType === 'editPass') || formType === 'addPass') {
                 const passwordField = form.querySelector('input[name="password"]');
-                alert(passwordField.value);
                 const dek = await getDek();
                 if (dek) {
                     const { encryptedData: arrPassword, iv: ivArrPass } = await encryptStringWithAesGcm(dek.dek, passwordField.value);
@@ -124,106 +106,106 @@ document.addEventListener('DOMContentLoaded', () => {
     )}
 );
 
-async function getDek() {
-    const { publicKey, privateKey } = await keyPairGenerate();
-    const salt = window.crypto.getRandomValues(new Uint8Array(16));
-    const saltB64 = window.btoa(String.fromCharCode.apply(null, salt));
-    const csrfToken = document.querySelector('input[name="csrf_token"]').value;
-    const iv = window.crypto.getRandomValues(new Uint8Array(12));
-    const data = await getEdek(publicKey, saltB64, csrfToken, iv);
-    const info = "ECDH AES-GCM"; // Ensure this info is the same on both client and server
-    if (data) {
-        // console.log("Complete data received:", data.serverPublicKey);
-        const sharedSecret = await getSharedSecret(privateKey, data.serverPublicKey);
-        const kek = await deriveAESKeyFromSharedSecret(sharedSecret.sharedSecret, salt, info) 
-        const dekArrayBuffer = await decryptData(kek, data.edek, iv);
-        const dek = await importAesKeyFromBuffer(dekArrayBuffer);
-        return {kek: kek, dek: dek}
-    }
-    return null
-}
+// async function getDek() {
+//     const { publicKey, privateKey } = await keyPairGenerate();
+//     const salt = window.crypto.getRandomValues(new Uint8Array(16));
+//     const saltB64 = window.btoa(String.fromCharCode.apply(null, salt));
+//     const csrfToken = document.querySelector('input[name="csrf_token"]').value;
+//     const iv = window.crypto.getRandomValues(new Uint8Array(12));
+//     const data = await getEdek(publicKey, saltB64, csrfToken, iv);
+//     const info = "ECDH AES-GCM"; // Ensure this info is the same on both client and server
+//     if (data) {
+//         // console.log("Complete data received:", data.serverPublicKey);
+//         const sharedSecret = await getSharedSecret(privateKey, data.serverPublicKey);
+//         const kek = await deriveAESKeyFromSharedSecret(sharedSecret.sharedSecret, salt, info) 
+//         const dekArrayBuffer = await decryptData(kek, data.edek, iv);
+//         const dek = await importAesKeyFromBuffer(dekArrayBuffer);
+//         return {kek: kek, dek: dek}
+//     }
+//     return null
+// }
 
-async function getEdek(publicKey, saltB64, csrfToken, iv) {
-    const ivB64 = arrayBufferToBase64(iv)
-    const confirmResponse = await fetch('/get_user_edek_iv', {
-        method: 'POST',  // Changed to GET since no body data is needed
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken  // CSRF token is still needed if your server requires it for all requests
-        },
-        body: JSON.stringify({publicKey: publicKey, salt: saltB64, iv: ivB64}),
-    });
-    if (confirmResponse.ok) {
-        const data = await confirmResponse.json();
-        const serverPublickKey = await importServerPublicKey(data.serverPubKeyB64);
-        const edek = base64ToArrayBuffer(data.edek);
-        return {edek: edek, serverPublicKey: serverPublickKey};
-    };
-    console.error('An error occurred');
-    return false 
-}
+// async function getEdek(publicKey, saltB64, csrfToken, iv) {
+//     const ivB64 = arrayBufferToBase64(iv)
+//     const confirmResponse = await fetch('/get_user_edek_iv', {
+//         method: 'POST',  // Changed to GET since no body data is needed
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'X-CSRFToken': csrfToken  // CSRF token is still needed if your server requires it for all requests
+//         },
+//         body: JSON.stringify({publicKey: publicKey, salt: saltB64, iv: ivB64}),
+//     });
+//     if (confirmResponse.ok) {
+//         const data = await confirmResponse.json();
+//         const serverPublickKey = await importServerPublicKey(data.serverPubKeyB64);
+//         const edek = base64ToArrayBuffer(data.edek);
+//         return {edek: edek, serverPublicKey: serverPublickKey};
+//     };
+//     console.error('An error occurred');
+//     return false 
+// }
 
-async function decryptData(key, encryptedData, iv) {
-    // Assume sharedSecret is already a CryptoKey suitable for decryption
-    if (!(key instanceof CryptoKey && key.usages.includes('decrypt'))) {
-        throw new Error("Provided kek is not a valid CryptoKey for decryption.");
-    }
-    try {
-        // Use the shared secret directly to decrypt the DEK
-        const decryptedData = await window.crypto.subtle.decrypt(
-            {
-                name: "AES-GCM",
-                iv: iv
-            },
-            key,
-            encryptedData);
-        console.log("Decryption successful");
-        return decryptedData; // Return the decrypted DEK for further use
-    } catch (error) {
-        console.error('Error occurred', error);
-        throw error;
-    }
-}
+// async function decryptData(key, encryptedData, iv) {
+//     // Assume sharedSecret is already a CryptoKey suitable for decryption
+//     if (!(key instanceof CryptoKey && key.usages.includes('decrypt'))) {
+//         throw new Error("Provided kek is not a valid CryptoKey for decryption.");
+//     }
+//     try {
+//         // Use the shared secret directly to decrypt the DEK
+//         const decryptedData = await window.crypto.subtle.decrypt(
+//             {
+//                 name: "AES-GCM",
+//                 iv: iv
+//             },
+//             key,
+//             encryptedData);
+//         console.log("Decryption successful");
+//         return decryptedData; // Return the decrypted DEK for further use
+//     } catch (error) {
+//         console.error('Error occurred', error);
+//         throw error;
+//     }
+// }
 
 
-async function encryptStringWithAesGcm(aesKey, passwordString) {
-    // Convert the password string to an ArrayBuffer (UTF-8)
-    const encoder = new TextEncoder(); // Encoder to convert string to Uint8Array
-    const passwordBytes = encoder.encode(passwordString); // Convert string to bytes
+// async function encryptStringWithAesGcm(aesKey, passwordString) {
+//     // Convert the password string to an ArrayBuffer (UTF-8)
+//     const encoder = new TextEncoder(); // Encoder to convert string to Uint8Array
+//     const passwordBytes = encoder.encode(passwordString); // Convert string to bytes
 
-    // Generate a random Initialization Vector (IV) for AES-GCM
-    const iv = window.crypto.getRandomValues(new Uint8Array(12)); // 12 bytes IV for AES-GCM
+//     // Generate a random Initialization Vector (IV) for AES-GCM
+//     const iv = window.crypto.getRandomValues(new Uint8Array(12)); // 12 bytes IV for AES-GCM
 
-    try {
-        // Encrypt the password bytes with the AES key
-        const encryptedData = await window.crypto.subtle.encrypt(
-            { name: "AES-GCM", iv },
-            aesKey,
-            passwordBytes // ArrayBuffer of password
-        );
+//     try {
+//         // Encrypt the password bytes with the AES key
+//         const encryptedData = await window.crypto.subtle.encrypt(
+//             { name: "AES-GCM", iv },
+//             aesKey,
+//             passwordBytes // ArrayBuffer of password
+//         );
 
-        return { encryptedData, iv };
-    } catch (error) {
-        console.error("Failed to encrypt data:", error);
-        throw error; // Rethrow to handle the error externally
-    }
-}
+//         return { encryptedData, iv };
+//     } catch (error) {
+//         console.error("Failed to encrypt data:", error);
+//         throw error; // Rethrow to handle the error externally
+//     }
+// }
 
-async function importAesKeyFromBuffer(arrayBuffer) {
-    try {
-        const key = await window.crypto.subtle.importKey(
-            "raw",  // format of the key to import
-            arrayBuffer,  // the ArrayBuffer containing the key data
-            {
-                name: "AES-GCM",
-                length: 256
-            },
-            false,  // whether the key is extractable
-            ["encrypt", "decrypt"]  // key usages
-        );
-        return key;
-    } catch (error) {
-        console.error("Error importing AES key from ArrayBuffer:", error);
-        throw error;  // Rethrow to maintain error context
-    }
-}
+// async function importAesKeyFromBuffer(arrayBuffer) {
+//     try {
+//         const key = await window.crypto.subtle.importKey(
+//             "raw",  // format of the key to import
+//             arrayBuffer,  // the ArrayBuffer containing the key data
+//             {
+//                 name: "AES-GCM",
+//                 length: 256
+//             },
+//             false,  // whether the key is extractable
+//             ["encrypt", "decrypt"]  // key usages
+//         );
+//         return key;
+//     } catch (error) {
+//         console.error("Error importing AES key from ArrayBuffer:", error);
+//         throw error;  // Rethrow to maintain error context
+//     }
+// }
